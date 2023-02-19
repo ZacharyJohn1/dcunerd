73749fb1abcfe2ad01d087d00bfb035093f0b83a
ZacharyJohn1 <zschroeder34@gmal.com>
2023-02-19 16:48:51 -0500

    added cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,85 @@
-
 export default function Home() {
   return (
     <main className="py-4 px-48">
-       <h1 className="bg-teal-700 text-black font-bold py-2 px-4 rounded-md">
-      Hello world!
-    </h1>
-<div className="p-6 shadow-lg rounded-lg bg-gray-100 text-gray-700 justify-content-center align-items-center">
-  <h2 className="font-semibold text-3xl mb-5">Hello world!</h2>
-  <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling extra attention
-    to featured content or information.
-  </p>
-  <hr className="my-6 border-gray-300" />
-  <p>
-    It uses utility classes for typography and spacing to space content out within the larger
-    container.
-  </p>
-  <button
-    type="button"
-    className="inline-block px-6 py-2.5 mt-4 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-    data-mdb-ripple="true"
-    data-mdb-ripple-color="light"
-  >
-    Learn more
-  </button>
-</div>
+      <div className="p-6 shadow-lg rounded-lg bg-gray-100 text-gray-700 text-center">
+        <h2 className="font-semibold text-3xl mb-5">Welcome to DCUNerds!</h2>
+        <p>
+          This is a place for fans of James Gunn's DC Universe to gather and
+          share ideas. I am so excited to grow alongside this amazing universe!
+        </p>
+      </div>
+      <div class="m-8 flex">
+        <div class="rounded-lg shadow-lg bg-white max-w-sm">
+          <a href="#!">
+            <img
+              class="rounded-t-lg"
+              src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+              alt=""
+            />
+          </a>
+          <div class="p-6">
+            <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+            <p class="text-gray-700 text-base mb-4">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <button
+              type="button"
+              class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Button
+            </button>
+          </div>
+        </div>
+        <div class="m-8 flex">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                class="rounded-t-lg"
+                src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                alt=""
+              />
+            </a>
+            <div class="p-6">
+              <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+              <p class="text-gray-700 text-base mb-4">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <button
+                type="button"
+                class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                Button
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="m-8 flex">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                class="rounded-t-lg"
+                src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                alt=""
+              />
+            </a>
+            <div class="p-6">
+              <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+              <p class="text-gray-700 text-base mb-4">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <button
+                type="button"
+                class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                Button
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
