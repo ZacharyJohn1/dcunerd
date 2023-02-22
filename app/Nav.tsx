f2895b294e4d68927925657b5e06b35477bcecc5
ZacharyJohn1 <zschroeder34@gmal.com>
2023-02-21 19:53:04 -0500

    fully mobile responsive homepage and navbar

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -2,39 +2,36 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav className="bg-white shadow dark:bg-gray-500 w-full h-[75px]">
-
-      <div className="container flex items-center justify-evenly p-6 mx-auto text-gray-600 h-[75px]">
-
+      <nav className='bg-white shadow dark:bg-gray-500 w-full h-[75px]'>
+        
+      <div className="sm:visible container flex items-center justify-evenly p-6 mx-auto text-gray-600 h-[75px]">
         <Link href="/">
-          <p className="text-xl font-semibold text-blue dark:text-black">
+          <p className="text-l font-semibold text-blue dark:text-black">
             Home
           </p>
         </Link>
 
         <Link href="/news">
-          <p className="text-xl font-semibold text-blue dark:text-black">
+          <p className="text-l font-semibold text-blue dark:text-black">
             News
           </p>
         </Link>
-        <a href="#">
+        <a href="# "className="visible flex order-first sm:order-none">
           <img src="mainlogo.png" className="rounded-full w-[70px] h-[70px]" />
         </a>
 
         <Link href="/reviews">
-          <p className="text-xl font-semibold text-blue dark:text-black">
+          <p className="text-l font-semibold text-blue dark:text-black">
             Reviews
           </p>
         </Link>
 
         <Link href="/timeline">
-          <p className="text-xl font-semibold text-blue dark:text-black">
+          <p className="text-l font-semibold text-blue dark:text-black">
             Timeline
           </p>
         </Link>
-
       </div>
-      
     </nav>
   );
 }
